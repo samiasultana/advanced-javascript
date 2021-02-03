8e87439b96445c3410ceb7b4639900381e14dd5d
unknown <samia361@gmail.com>
2021-02-03 16:40:40 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+let name = "false";
+if (name) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
+// empty string "" , 0, Null, NaN, undefined(let name;) shudhu false, onno sob true.
+// ""
+// 0
+// undefined
+// null
+// NaN
+// false
+
+// truthy: " ", "0", [], {}, true, "false"
