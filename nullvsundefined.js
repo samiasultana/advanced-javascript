617423862c3214a9992455c5e3fb49951629c535
unknown <samia361@gmail.com>
2021-02-03 22:31:07 +0600

    nullvsundefined

---
+++ nullvsundefined.js
@@ -0,0 +1,16 @@
+// let pakhi;
+function add(num1,num2=1){
+    console.log(num1+num2);
+    // return num1+num2;
+}
+const result=add(12);
+console.log(result);
+
+const bf={name: "sabun", phone:01714834873};
+console.log(bf.phone);
+
+let fun
+console.log(fun)
+
+let ages=[12,23]
+console.log(ages[22]);
