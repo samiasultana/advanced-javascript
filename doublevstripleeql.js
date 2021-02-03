89ded8a709f2865c0ec3a8cf7ce3e000213c3d3b
unknown <samia361@gmail.com>
2021-02-03 22:47:42 +0600

    doubletripple

---
+++ doublevstripleeql.js
@@ -0,0 +1,11 @@
+const first = 0;
+const sec = false;
+if (first === sec) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false")
+}
+
+// == only checks value, === checks both the value and type.
+// == e 0 ar false 2ta const ke compare korle true, 1 and true ke compare korleo true dekhabe, tai compare korte always use ===
